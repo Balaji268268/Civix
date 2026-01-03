86b1c7922bb150e3c53ac3d9ec0e1d06e597db88
Ananthagari Venkata Balaji <galigastagiramma@gmail.com>
2026-01-03 11:33:48 +0530

    Fix merge conflict in admin routes

--- backend/routes/admin.js
+++ backend/routes/admin.js
@@ -1,30 +1,20 @@
 const express = require('express');
 const router = express.Router();
-<<<<<<< HEAD
 const { getAdminStats, findDuplicatesForIssue, getAllUsers, getUserDetails, approveUser, getCommunityInsights, getSettings, updateSettings, exportSystemData } = require('../controllers/adminController');
-const { verifyToken, isAdmin } = require('../middlewares/validate');
-
-router.get('/stats', getAdminStats);
-router.get('/check-duplicates/:id', findDuplicatesForIssue);
-router.get('/users', getAllUsers);
-router.get('/users/:id', getUserDetails);
-router.patch('/users/:id/approve', approveUser);
-
-router.get('/community-insights', verifyToken, isAdmin, getCommunityInsights);
-
-// Settings & Export
-router.get('/settings', getSettings);
-router.patch('/settings', updateSettings);
-router.get('/export', exportSystemData);
-=======
-const { getAdminStats, findDuplicatesForIssue } = require('../controllers/adminController');
 const { verifyToken, isAdmin } = require('../middlewares/validate');
 
 router.get('/stats', verifyToken, isAdmin, getAdminStats);
 router.get('/check-duplicates/:id', verifyToken, isAdmin, findDuplicatesForIssue);
-const { getAllUsers, getUserDetails } = require('../controllers/adminController');
+
 router.get('/users', verifyToken, isAdmin, getAllUsers);
 router.get('/users/:id', verifyToken, isAdmin, getUserDetails);
->>>>>>> 6dfaa0f0271f642bfb702ab31aa972d1c7f0668a
+router.patch('/users/:id/approve', verifyToken, isAdmin, approveUser);
+
+router.get('/community-insights', verifyToken, isAdmin, getCommunityInsights);
+
+// Settings & Export
+router.get('/settings', verifyToken, isAdmin, getSettings);
+router.patch('/settings', verifyToken, isAdmin, updateSettings);
+router.get('/export', verifyToken, isAdmin, exportSystemData);
 
 module.exports = router;
